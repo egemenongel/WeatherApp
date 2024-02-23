b6d55730559b74fe4e51de1dbb22e7085b4f8dcd
egemenongel <egemenongel@hotmail.com>
2024-02-23 22:16:33 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Egemen Öngel on 23.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
